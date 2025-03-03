70c99464e3a914f23471d8ec11e734a6fcfb3650
Aki Sasaki <aki@escapewindow.com>
2025-03-03 14:58:38 -0700

    initial commit - skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Markdown Novel tools
+
+Tools I use in my [Obsidian](https://obsidian.md) markdown novel.
---
+++ setup.py
@@ -0,0 +1,54 @@
+import datetime
+import json
+import os
+from pathlib import Path
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+project_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+# Using CalVer https://calver.org/
+VERSION = datetime.date.today().strftime("%y.%m.%d")
+
+tests_require = [
+    "flake8",
+    "flake8_docstrings",
+    "pytest",
+]
+
+with open(project_dir / "requirements.txt") as f:
+    install_requires = f.readlines()
+
+with open(project_dir / "README.md") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="markdown-novel-tools",
+    version=VERSION,
+    description="Markdown Novel Tools",
+    long_description=long_description,
+    author="Aki Sasaki",
+    author_email="aki@escapewindow.com",
+    url="https://github.com/escapewindow/markdown-novel-tools",
+    packages=["markdown-novel-tools"],
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+    zip_safe=False,
+    license="MPL 2.0",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    python_requires=">=3.7",
+    classifiers=(
+        "Intended Audience :: Other Audience",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ),
+)
