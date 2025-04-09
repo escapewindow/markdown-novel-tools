a55728208d327d4e47e1394ce09d47a51c9162a9
Aki Sasaki <aki@escapewindow.com>
2025-04-09 12:17:21 -0600

    get_new_config_val tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+import pytest
+
 import markdown_novel_tools.config as mdconfig
 
 
@@ -11,3 +13,20 @@ def test_get_primary_outline_path():
     }
     expected = Path("/tmp/foo")
     assert mdconfig.get_primary_outline_path(config) == expected
+
+
+@pytest.mark.parametrize(
+    "config_val, user_config_val, key_name, raises, expected",
+    (
+        ("a", None, "", False, "a"),
+        ("a", "b", "", False, "b"),
+        (["a"], ["b", "c"], "", False, ["b", "c"]),
+        ({"a": "foo", "b": "bar"}, {"a": "baz"}, "", False, {"a": "baz", "b": "bar"}),
+    ),
+)
+def test_get_new_config_val(config_val, user_config_val, key_name, raises, expected):
+    if raises:
+        with pytest.raises(raises):
+            md_config._get_new_config_val(config_val, user_config_val, key_name)
+    else:
+        assert mdconfig._get_new_config_val(config_val, user_config_val, key_name) == expected
