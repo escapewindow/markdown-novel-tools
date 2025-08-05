dcb5e656a27ba41503f64df9d9a1bd6e76d8bde4
Aki Sasaki <aki@escapewindow.com>
2025-08-04 18:36:04 -0600

    fix epub scene split

--- src/markdown_novel_tools/constants.py
+++ src/markdown_novel_tools/constants.py
@@ -72,6 +72,6 @@ SCENE_SPLIT_REGEX = re.compile(r"""^\s{4,}\* \* \*""")
 TABLE_DIVIDER_REGEX = re.compile(r"""^[|\-\s]*$""")
 
 # Strings {{{1
-SCENE_SPLIT_ASTERISK = "&ast;&nbsp;&nbsp;&nbsp;&ast;&nbsp;&nbsp;&nbsp;&ast;"
+SCENE_SPLIT_ASTERISK = r"\n\n<br /><br /><center>\n&ast;&nbsp;&nbsp;&nbsp;&ast;&nbsp;&nbsp;&nbsp;&ast;\n</center><br /><br />\n\n"
 SCENE_SPLIT_PLAINTEXT = "    * * *"
 SCENE_SPLIT_POUND = f"{'&nbsp;' * 60}#"
