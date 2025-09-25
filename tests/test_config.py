bbfc6a4ee7c8ec81c320178e55e8107061f2ebd4
Aki Sasaki <aki@escapewindow.com>
2025-09-24 19:25:41 -0600

    todo for test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,8 +3,36 @@
 from pathlib import Path
 
 import pytest
+from git import Repo
 
 import markdown_novel_tools.config as mdconfig
+
+
+def test_get_config_path():
+    """
+    TODO
+    - set XDG_CONFIG_HOME and HOME to empty/nonexistent dirs, make sure return value is None
+    - create bogus git repo with a .config.yaml, cd into there, make sure we get that
+    - set XDG_CONFIG_HOME to a dir with a .config.yaml, make sure we get that
+    - set HOME to that dir, make sure we get that
+    - set all of them, make sure we get the git one
+
+    Do I want a git repo for any other tests? I could create a bogus one as a fixture
+    """
+
+
+#    try:
+#        git_repo = Repo(Path("."), search_parent_directories=True)
+#        git_root = Path(git_repo.git.rev_parse("--show-toplevel"))
+#        search_path.append(git_root / ".config.yaml")
+#    except InvalidGitRepositoryError:
+#        pass
+#    if "XDG_CONFIG_HOME" in os.environ:
+#        search_path.append(Path(os.environ["XDG_CONFIG_HOME"]) / "md-novel" / "novel-config.yaml")
+#    search_path.append(Path(os.environ["HOME"]) / ".novel_config.yaml")
+#    for path in search_path:
+#        if os.path.exists(path):
+#            return path
 
 
 def test_get_primary_outline_path():
