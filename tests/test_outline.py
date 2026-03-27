2e38d15c334a305d177d85253ef9f005db9eb823
Aki Sasaki <aki@escapewindow.com>
2026-03-27 12:05:37 -0600

    start testing outline

--- tests/test_outline.py
+++ tests/test_outline.py
@@ -3,5 +3,24 @@
 import markdown_novel_tools.outline as outline
 
 
-def test_outline():
+def test_table():
     pass
+
+
+def test_outline_to_yaml():
+    pass
+
+
+def test_get_line_parts():
+    pass
+
+
+def test_get_outline_file_header():
+    assert outline.get_outline_file_header("arcs") == """---
+tags: ['arcs', 'outline']
+aliases: []
+---
+
+# Arcs
+
+"""
