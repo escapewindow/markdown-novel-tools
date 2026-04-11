0b80f492cf263f9abb9c39a0b5613bded44d01de
Aki Sasaki <aki@escapewindow.com>
2026-04-11 08:42:11 -0600

    get_css_path

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 """Test constants."""
 
 import os
+import re
 import tempfile
 from pathlib import Path
 
@@ -10,17 +11,12 @@ from git import Repo
 import markdown_novel_tools.config as mdconfig
 
 
-def test_get_config_path():
-    """
-    TODO
-    - set XDG_CONFIG_HOME and HOME to empty/nonexistent dirs, make sure return value is None
-    - create bogus git repo with a .config.yaml, cd into there, make sure we get that
-    - set XDG_CONFIG_HOME to a dir with a .config.yaml, make sure we get that
-    - set HOME to that dir, make sure we get that
-    - set all of them, make sure we get the git one
+def hack_private_var(path):
+    """Mac /private/var is /var, but git.Repo returns one and Path returns the other"""
+    return re.sub("^/private/var/", "/var/", str(path))
 
-    Do I want a git repo for any other tests? I could create a bogus one as a fixture
-    """
+
+def test_get_config_path():
     with tempfile.TemporaryDirectory() as tmp_home, tempfile.TemporaryDirectory() as tmp_repo, tempfile.TemporaryDirectory() as tmp_config:
         os.environ = {
             "HOME": tmp_home,
@@ -40,22 +36,21 @@ def test_get_config_path():
         home_config.touch()
         assert mdconfig.get_config_path() == home_config
 
-        os.makedirs(Path(tmp_repo) / "parent" / "child")
-        # assert Path(".") / ".config.yaml" == mdconfig.get_config_path()
+        # config config
+        config_config = Path(tmp_config) / "md-novel" / "novel-config.yaml"
+        os.mkdir(config_config.parent)
+        config_config.touch()
+        assert mdconfig.get_config_path() == config_config
 
-
-#    try:
-#        git_repo = Repo(Path("."), search_parent_directories=True)
-#        git_root = Path(git_repo.git.rev_parse("--show-toplevel"))
-#        search_path.append(git_root / ".config.yaml")
-#    except InvalidGitRepositoryError:
-#        pass
-#    if "XDG_CONFIG_HOME" in os.environ:
-#        search_path.append(Path(os.environ["XDG_CONFIG_HOME"]) / "md-novel" / "novel-config.yaml")
-#    search_path.append(Path(os.environ["HOME"]) / ".novel_config.yaml")
-#    for path in search_path:
-#        if os.path.exists(path):
-#            return path
+        repo_path = Path(tmp_repo)
+        os.makedirs(repo_path / "parent" / "child")
+        repo_config_path = repo_path / ".config.yaml"
+        repo_config_path.touch()
+        for path in (repo_path, repo_path / "parent", repo_path / "parent" / "child"):
+            os.chdir(path)
+            assert hack_private_var(mdconfig.get_config_path()) == hack_private_var(
+                repo_config_path
+            )
 
 
 def test_get_primary_outline_path():
@@ -64,6 +59,62 @@ def test_get_primary_outline_path():
     }
     expected = Path("/tmp/foo")
     assert mdconfig.get_primary_outline_path(config) == expected
+
+
+@pytest.mark.parametrize(
+    "format_, expected",
+    (
+        ("one", "one_path"),
+        ("two", "two_path"),
+        (None, "default_path"),
+        ("nonexistent", "default_path"),
+    ),
+)
+def test_get_metadata_path(format_, expected):
+    config = {
+        "convert": {
+            "metadata_path": {
+                "one": "one_path",
+                "two": "two_path",
+                "default": "default_path",
+            },
+        }
+    }
+    kwargs = {}
+    if format_ is not None:
+        kwargs["format_"] = format_
+    assert mdconfig.get_metadata_path(config, **kwargs) == Path(expected)
+
+
+@pytest.mark.parametrize(
+    "variant, expected, raises",
+    (
+        (None, "css_dir/pdf_path", None),
+        ("manuscript_pdf_css_path", "css_dir/pdf_path", None),
+        ("one", "css_dir/one_path", None),
+        ("two", "css_dir/two_path", None),
+        ("nonexistent", None, KeyError),
+    ),
+)
+def test_get_css_path(variant, expected, raises):
+    config = {
+        "convert": {
+            "css": {
+                "css_dir": "css_dir",
+                "manuscript_pdf_css_path": "pdf_path",
+                "one": "one_path",
+                "two": "two_path",
+            },
+        }
+    }
+    kwargs = {}
+    if variant is not None:
+        kwargs["variant"] = variant
+    if raises:
+        with pytest.raises(raises):
+            mdconfig.get_css_path(config, **kwargs)
+    else:
+        assert mdconfig.get_css_path(config, **kwargs) == Path(expected)
 
 
 @pytest.mark.parametrize(
