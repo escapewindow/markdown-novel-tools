4d444e103bd86f65e1782fb8cb221b84c2a313ec
Aki Sasaki <aki@escapewindow.com>
2026-04-30 13:03:30 -0600

    start novel test file

---
+++ tests/test_novel.py
@@ -0,0 +1,63 @@
+"""Test novel."""
+
+import pytest
+
+import markdown_novel_tools.novel as novel
+
+from . import TEST_DATA_DIR
+
+
+def test__beats_helper():
+    pass
+
+
+def test__write_to_file_helper():
+    pass
+
+
+def test_novel_beats():
+    pass
+
+
+def test_novel_convert():
+    pass
+
+
+def test_novel_lint():
+    pass
+
+
+def test_novel_links():
+    pass
+
+
+def test_novel_new():
+    pass
+
+
+def test_novel_outline_convert():
+    pass
+
+
+def test_novel_stats():
+    pass
+
+
+def test_arc_grep():
+    pass
+
+
+def test_novel_sync():
+    pass
+
+
+def test_novel_today():
+    pass
+
+
+def test_novel_parser():
+    pass
+
+
+def test_novel_tool():
+    pass
